6ff02d0748c1053dc39bdf63ed90476e51b14a11
feather <Brian608@163.com>
2022-06-03 13:52:13 +0800

    异常处理

--- dev-service/src/main/java/org/feather/handler/CommonGlobalExceptionHandler.java
+++ dev-service/src/main/java/org/feather/handler/CommonGlobalExceptionHandler.java
@@ -4,11 +4,17 @@ import org.feather.common.JsonResponse;
 import org.feather.exception.ConditionException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @program: dev-common
@@ -19,6 +25,26 @@ import javax.servlet.http.HttpServletRequest;
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CommonGlobalExceptionHandler {
+
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public JsonResponse<String> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex) {
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> errors = bindingResult.getFieldErrors();
+        //初始化错误信息大小
+        Map<String, String> errorMessage = new HashMap<>(errors.size());
+        for (FieldError error : errors
+        ) {
+            errorMessage.put(error.getField(), error.getDefaultMessage());
+        }
+        JsonResponse<String> jsonResponse = new JsonResponse<>();
+        jsonResponse.setMsg(errorMessage.toString());
+        jsonResponse.setCode("500");
+        jsonResponse.setData(null);
+        return jsonResponse;
+    }
 
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
