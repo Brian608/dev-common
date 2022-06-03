5b1a3cd043bd952a4df05d9d9e70cd12185b7364
feather <Brian608@163.com>
2022-06-03 09:11:08 +0800

    project init

---
+++ dev-api/src/main/java/org/feather/DevCommonApplication.java
@@ -0,0 +1,20 @@
+package org.feather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @projectName: dev-common
+ * @package: org.feather
+ * @className: DevCommonApplication
+ * @author: feather(杜雪松)
+ * @description: TODO
+ * @since: 2022/6/3 08:45
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class DevCommonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevCommonApplication.class,args);
+    }
+}
